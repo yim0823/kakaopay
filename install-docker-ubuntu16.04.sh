54085fff9a46177497fc2c9fe73a5b28a4bc4b09
yim0823 <yim0823@naver.com>
2019-04-22 04:24:23 +0900

    Update how to install docker with apt-get
    도커 문서에 나오는 방법으로 변경해 적용

--- install-docker-ubuntu16.04.sh
+++ install-docker-ubuntu16.04.sh
@@ -4,11 +4,12 @@ set -xe
 
 # Update apt sources
 sudo apt-get update
-sudo apt-get install apt-transport-https ca-certificates
+sudo apt-get install apt-transport-https ca-certificates curl gnupg-agent software-properties-common
 
-# Add GPG key
-sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
-sudo sh -c 'echo "deb https://apt.dockerproject.org/repo ubuntu-xenial main" > /etc/apt/sources.list.d/docker.list'
+# Add Docker’s official GPG key
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo apt-key fingerprint 0EBFCD88 | grep docker@docker.com || exit 1
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu xenial stable" | tee /etc/apt/sources.list.d/docker.list
 
 # Upgrades apt packages
 sudo apt-get update
@@ -23,25 +24,25 @@ sudo apt-cache policy docker-engine
 sudo apt-get install -y linux-image-extra-$(uname -r)
 
 # Install docker python-pip
-sudo apt-get install -y docker-engine
+sudo apt-get install -y docker-ce
 
 # Install basic packages
-sudo apt-get install -y python-pip git curl build-essential
+sudo apt-get install -y python-pip git build-essential
 
 # Start docker & create group & add group account
 sudo service docker start
 sudo systemctl enable docker
 sudo groupadd docker
 sudo usermod -aG docker $(whoami)
+sudo systemctl restart docker
 
-echo "Docker engine installed"
+sudo systemctl start docker.service
+sudo systemctl enable docker.service
 
-COMPOSE_VERSION=`git ls-remote https://github.com/docker/compose | grep refs/tags | grep -oP "[0-9]+\.[0-9]+\.[0-9]+$" | tail -n 1`
-sudo sh -c "curl -L https://github.com/docker/compose/releases/download/${COMPOSE_VERSION}/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose"
-sudo chmod +x /usr/local/bin/docker-compose
-sudo sh -c "curl -L https://raw.githubusercontent.com/docker/compose/${COMPOSE_VERSION}/contrib/completion/bash/docker-compose > /etc/bash_completion.d/docker-compose"
+echo "Docker-ce installed"
 
-echo "Docker compose ${COMPOSE_VERSION} installed"
+sudo apt-get install -y docker-compose
+echo "Docker compose installed"
 
 # Create directory and clone git repository
 mkdir -p /usr/app
