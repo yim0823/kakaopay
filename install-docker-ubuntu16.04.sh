3c456b2e5a551baa5c5736bd28330c0bed4e3309
yim0823 <yim0823@naver.com>
2019-04-22 20:14:03 +0900

    permission denied으로 인해 적용 안되서 변경작업 진행

--- install-docker-ubuntu16.04.sh
+++ install-docker-ubuntu16.04.sh
@@ -9,8 +9,8 @@ sudo apt-get install apt-transport-https ca-certificates curl gnupg-agent softwa
 # Add Docker’s official GPG key
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo apt-key fingerprint 0EBFCD88 | grep docker@docker.com || exit 1
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu xenial stable" | tee /etc/apt/sources.list.d/docker.list
-
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+ 
 # Upgrades apt packages
 sudo apt-get update
 
