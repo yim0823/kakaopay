d685c67356beaae9eeebe8e7768adddff9fb9354
yim0823 <yim0823@naver.com>
2019-04-22 20:40:59 +0900

    docker compose 설치 변경

--- install-docker-ubuntu16.04.sh
+++ install-docker-ubuntu16.04.sh
@@ -41,7 +41,15 @@ sudo systemctl enable docker.service
 
 echo "Docker-ce installed"
 
-sudo apt-get install -y docker-compose
+# download the current stable release of Docker Compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.24.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+
+# Apply executable permissions to the binary
+sudo chmod +x /usr/local/bin/docker-compose
+
+# make link
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+
 echo "Docker compose installed"
 
 # Create directory and clone git repository
