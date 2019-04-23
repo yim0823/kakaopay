a729b3b10c02d76109486e1c26e814d6a7e7e368
yim0823 <yim0823@naver.com>
2019-04-24 05:46:06 +0900

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,26 @@
 #!/bin/bash
 
+REPOSITORY=/usr/app
+SERVICE_NAME=kakaopay
+PROJECT_NAME=spring-boot-sample-web-ui
+
 TARGET_DEPLOY_TCP=tcp://13.124.96.59:2375
 DOCKER_APP_NAME=kakaopay_server
+
+cd $REPOSITORY
+
+echo "Git Pull"
+
+git pull
+
+echo "Start Build"
+
+cd $REPOSITORY/$SERVICE_NAME/$PROJECT_NAME
+./gradlew build
+
+echo "Copy Build file"
+
+cp ./build/libs/*.jar $REPOSITORY/
 
 EXIST_BLUE=$(DOCKER_HOST=${TARGET_DEPLOY_TCP} docker-compose -p ${DOCKER_APP_NAME}-blue -f docker-compose.blue.yml ps | grep Up)
 
