5117936176d9a3a45f48717d62466408ad88d150
yim0823 <yim0823@naver.com>
2019-04-24 05:32:08 +0900

    내용 정정

--- deploy.sh
+++ deploy.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
 
-DOCKER_APP_NAME=node-koa-server
+TARGET_DEPLOY_TCP=tcp://13.124.96.59:2375
+DOCKER_APP_NAME=kakaopay_server
 
-EXIST_BLUE=$(docker-compose -p ${DOCKER_APP_NAME}-blue -f docker-compose.blue.yml ps | grep Up)
+EXIST_BLUE=$(DOCKER_HOST=${TARGET_DEPLOY_TCP} docker-compose -p ${DOCKER_APP_NAME}-blue -f docker-compose.blue.yml ps | grep Up)
 
 if [ -z "$EXIST_BLUE" ]; then
     echo "blue up"
-    docker-compose -p ${DOCKER_APP_NAME}-blue -f docker-compose.blue.yml up -d
+    DOCKER_HOST=${TARGET_DEPLOY_TCP} docker-compose -p ${DOCKER_APP_NAME}-blue -f docker-compose.blue.yml up --build
 
     sleep 10
 
-    docker-compose -p ${DOCKER_APP_NAME}-green -f docker-compose.green.yml down
+    DOCKER_HOST=${TARGET_DEPLOY_TCP} docker-compose -p ${DOCKER_APP_NAME}-green -f docker-compose.green.yml down
 else
     echo "green up"
-    docker-compose -p ${DOCKER_APP_NAME}-green -f docker-compose.green.yml up -d
+    DOCKER_HOST=${TARGET_DEPLOY_TCP} docker-compose -p ${DOCKER_APP_NAME}-green -f docker-compose.green.yml up --build
 
     sleep 10
 
-    docker-compose -p ${DOCKER_APP_NAME}-blue -f docker-compose.blue.yml down
+    DOCKER_HOST=${TARGET_DEPLOY_TCP} docker-compose -p ${DOCKER_APP_NAME}-blue -f docker-compose.blue.yml down
 fi
