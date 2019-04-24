f787a2a152d7d52dc3d7354dc30a3320bd7db96b
yim0823 <yim0823@naver.com>
2019-04-24 23:43:16 +0900

    Modify how to run docker cli
    docker remote api -> docker cli
    It changed the way from remote execution to local execution.

--- deploy.sh
+++ deploy.sh
@@ -25,20 +25,20 @@ echo "Start Build"
 cd $REPOSITORY/$SERVICE_NAME/$PROJECT_NAME
 ./gradlew build
 
-EXIST_BLUE=$(DOCKER_HOST=${TARGET_DEPLOY_TCP} docker-compose -p ${DOCKER_APP_NAME}-blue -f docker-compose.blue.yml ps | grep Up)
+EXIST_BLUE=$(docker-compose -p ${DOCKER_APP_NAME}-blue -f docker-compose.blue.yml ps | grep Up)
 
 if [ -z "$EXIST_BLUE" ]; then
     echo "blue up"
-    DOCKER_HOST=${TARGET_DEPLOY_TCP} docker-compose -p ${DOCKER_APP_NAME}-blue -f docker-compose.blue.yml up --build
+    docker-compose -p ${DOCKER_APP_NAME}-blue -f docker-compose.blue.yml up -d
 
     sleep 10
 
-    DOCKER_HOST=${TARGET_DEPLOY_TCP} docker-compose -p ${DOCKER_APP_NAME}-green -f docker-compose.green.yml down
+    docker-compose -p ${DOCKER_APP_NAME}-green -f docker-compose.green.yml down
 else
     echo "green up"
-    DOCKER_HOST=${TARGET_DEPLOY_TCP} docker-compose -p ${DOCKER_APP_NAME}-green -f docker-compose.green.yml up --build
+    docker-compose -p ${DOCKER_APP_NAME}-green -f docker-compose.green.yml up -d
 
     sleep 10
 
-    DOCKER_HOST=${TARGET_DEPLOY_TCP} docker-compose -p ${DOCKER_APP_NAME}-blue -f docker-compose.blue.yml down
+    docker-compose -p ${DOCKER_APP_NAME}-blue -f docker-compose.blue.yml down
 fi
