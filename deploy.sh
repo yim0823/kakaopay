a4aad09af84ee26b4a6e73d827334d2a4b52ca08
yim0823 <yim0823@naver.com>
2019-04-24 23:44:52 +0900

    Change existed comment

--- deploy.sh
+++ deploy.sh
@@ -1,17 +1,13 @@
 #!/bin/bash
 
-##########################################################################################
+###################################################
 # This file is for non-disruptive deployment.
-# You have to change a variable's value, TARGET_DEPLOY_TCP, to your docker machine 
-# when using the script. In order to execute the docker CLI, It uses Docker remote API 
-# in this script.
-##########################################################################################
+###################################################
 
 REPOSITORY=/usr/app
 SERVICE_NAME=kakaopay
 PROJECT_NAME=spring-boot-sample-web-ui
 
-TARGET_DEPLOY_TCP=tcp://13.124.96.59:2375
 DOCKER_APP_NAME=kakaopay_server
 
 cd $REPOSITORY
