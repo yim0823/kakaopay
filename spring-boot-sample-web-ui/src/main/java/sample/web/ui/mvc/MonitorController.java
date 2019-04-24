c213ca560046f5c51abf3c6e84de4fb2b1e041a5
gtrus <gtrus@YIM-LAPTOP>
2019-04-25 04:15:24 +0900

    uri 변경

--- spring-boot-sample-web-ui/src/main/java/sample/web/ui/mvc/MonitorController.java
+++ spring-boot-sample-web-ui/src/main/java/sample/web/ui/mvc/MonitorController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/monitor")
+@RequestMapping("/health")
 public class MonitorController {
 
 	public Logger logger = LoggerFactory.getLogger(MonitorController.class);
